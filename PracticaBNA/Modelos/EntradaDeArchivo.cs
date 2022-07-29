a2f9c2526159f86dce2eb1b19e52c1c1d54f902d
BrunoLoFaro <lofarobruno@gmail.com>
2022-07-29 12:26:05 -0300

    Interfaz de Entrada y clase de entrada de archivo

---
+++ PracticaBNA/Modelos/EntradaDeArchivo.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace PracticaBNA.Modelos
+{
+    internal class FileInput : IEntrada
+    {
+        StreamReader sr;
+        public FileInput(string fileDir)
+        {
+            if (!File.Exists(fileDir))
+                throw new FileNotFoundException("Invalid file.");
+            sr = new StreamReader(fileDir);
+        }
+
+        public string ObtenerLinea()
+        {
+            string input;
+            input = sr.ReadLine();
+            if (input == null)
+                sr.Close();
+            return input;
+        }
+    }
+}
---
+++ PracticaBNA/Modelos/IEntrada.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PracticaBNA
+{
+    interface IEntrada
+    {
+        string ObtenerLinea();
+    }
+}
