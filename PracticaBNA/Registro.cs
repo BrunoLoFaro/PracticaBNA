98ba7bc672cf092eb58047712207b28b795c8e27
BrunoLoFaro <lofarobruno@gmail.com>
2022-07-29 09:23:20 -0300

    constructor de registro

---
+++ PracticaBNA/Registro.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaBNA
+{
+    internal class Registro
+    {
+        private string fechaString;
+        public DateTime fecha;
+        private double temperatura;
+        private double humedad;
+        private string codigo;
+        private byte estado;
+
+        public Registro(string entrada)
+        {
+            try
+            {
+                fechaString = entrada.Substring(0, 14);
+                fecha = DateTime.ParseExact(fechaString, "yyyyMMddHHmmss", null);
+                temperatura = double.Parse(entrada.Substring(14, 3)) / 10;
+                if (temperatura < 0)
+                    throw new FormatException("temperatura no puede ser negativa");
+                humedad = double.Parse(entrada.Substring(17, 3)) / 10;
+                if (humedad < 0)
+                    throw new FormatException("humedad no puede ser negativa");
+                codigo = entrada.Substring(20, 4);
+                estado = byte.Parse(entrada.Substring(24));
+                if (estado > 1 || estado < 0)
+                    throw new FormatException("estado debe ser 0 o 1");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("No se pudieron parsear los datos.\n" + e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+    }
+}
