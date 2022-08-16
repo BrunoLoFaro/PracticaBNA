1c9f6a16a7979f5b3c92573bdfcb0da23b2a943a
BrunoLoFaro <blofaro@bna.com.ar>
2022-08-16 16:09:23 -0300

    Eliminado de dependencias

--- PracticaBNA/Modelos/Registro.cs
+++ PracticaBNA/Modelos/Registro.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PracticaBNA.Utils;
+﻿using PracticaBNA.Utils;
 
 namespace PracticaBNA.Modelos
 {
--- PracticaBNA/Utils/Utilidades.cs
+++ PracticaBNA/Utils/Utilidades.cs
@@ -1,7 +1,4 @@
 ﻿using PracticaBNA.Modelos;
-using System;
-using System.Collections.Generic;
-using System.IO;
 
 namespace PracticaBNA.Utils
 {
